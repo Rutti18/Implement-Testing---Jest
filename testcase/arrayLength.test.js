d430095ffd72a9ddd0a7d3b0e14b6c645a6651fd
Rutti <ruttib@gmail.com>
2022-04-14 22:54:45 -0400

    updated

--- testcase/arrayLength.test.js
+++ testcase/arrayLength.test.js
@@ -1,9 +1,5 @@
 const array = require('../arrayLength');
 
-/*
-Use .toHaveLength to check that an object has a .length property and it is set to a certain numeric value.
-This is especially useful for checking arrays or strings size.
-*/
 var app = [1,2,3,4,5];
 test('array value should be equal to', () => {
     expect(array(app)).toHaveLength(5);
